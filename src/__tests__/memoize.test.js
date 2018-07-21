5abeb1d4d4ff188ec1cd50b84ada09238233c973
Lam Hieu <lamhieu.vk@gmail.com>
2018-07-21 10:23:27 +0700

    update test cases

--- src/__tests__/memoize.test.js
+++ src/__tests__/memoize.test.js
@@ -2,16 +2,14 @@
 
 import memoize from 'memoize'
 
-describe('success', () => {
+describe('memoize', () => {
   it('handle', () => {
     const fn = (a, b) => a + b
     const handle = memoize(fn)
     const results = 3
     expect(handle(1, 2)).toEqual(results)
   })
-})
 
-describe('fail', () => {
   it('first argument not is function', () => {
     const fn = null
     const handle = memoize(fn)
